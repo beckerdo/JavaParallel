c7df97193b2adfb520e8c3b8f572d46cf828dbfd
Dan Becker <dabecker@paypal.com>
2015-06-17 12:51:15 -0500

    Show ExecutorService shutdownNow

--- src/main/java/com/example/ParallelSite.java
+++ src/main/java/com/example/ParallelSite.java
@@ -1,9 +1,9 @@
 package com.example;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -15,6 +15,7 @@ import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.Random;
 
 /**
  * Ping N web sites in parallel. The ping simply does a GET, and looks at the
@@ -39,6 +40,7 @@ public final class ParallelSite {
 		"http://www.ebay.com", "http://www.paypal.com",
 		"http://www.apache.org", "http://www.github.com"
 	);
+	public static final Random random = new Random();
 	
 	/** Run this tool. */
 	public static final void main(String... aArgs) {
@@ -52,6 +54,9 @@ public final class ParallelSite {
 
 			log("Sequential, report each as it completes:");
 			checker.pingAndReportSequentially();
+
+			log("Parallel, terminate when one breaks:");
+			checker.pingAndReportEachWhenKnownTerminateOnFail();
 		} catch (InterruptedException ex) {
 			Thread.currentThread().interrupt();
 		} catch (ExecutionException ex) {
@@ -73,8 +78,8 @@ public final class ParallelSite {
 		long start = System.currentTimeMillis();
 		int numThreads = URLs.size() > MAX_THREADS ? MAX_THREADS : URLs.size(); 
 		ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-		CompletionService<PingResult> completionService = new ExecutorCompletionService<>(
-				executor);
+		CompletionService<PingResult> completionService = 
+			new ExecutorCompletionService<>(executor);
 		for (String url : URLs) {
 			Task task = new Task(url);
 			completionService.submit(task);
@@ -126,7 +131,56 @@ public final class ParallelSite {
 		log("Duration: " + duration + " mS");
 	}
 
+	/**
+	 * Check N sites, in parallel, using up to MAX_THREADS
+	 * <p> 
+	 * Uses fixed thread executor service and completion service to
+	 * report the result of each 'ping' as it comes in.
+	 * <p>
+	 * A random URL failure is injected (mis-spelled URL).
+	 * The task will fail when this is encountered.
+	 */
+	void pingAndReportEachWhenKnownTerminateOnFail() throws InterruptedException,
+			ExecutionException {
+		long start = System.currentTimeMillis();
+		int numThreads = URLs.size() > MAX_THREADS ? MAX_THREADS : URLs.size(); 
+		ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+		CompletionService<PingResult> completionService = 
+			new ExecutorCompletionService<>(executor);
+		int brokenURL = random.nextInt( URLs.size( ));
+		for (int i = 0; i < URLs.size(); i++) {
+			String urlString = URLs.get(i);
+			if ( i == 0 ) {
+				urlString = breakString( 7, 3, urlString );
+			}
+			Task task = new Task(urlString);
+			completionService.submit(task);
+		}
+		for (String url : URLs) {
+			Future<PingResult> future = completionService.take();
+			PingResult result = future.get();
+			log(result);
+			if ( !result.success ) {
+				List<Runnable> waiting = executor.shutdownNow();
+				log( "Shutdown called. Waiting task count=" + waiting.size());
+			}
+		}
+		executor.shutdown(); // always reclaim resources
+		long duration = System.currentTimeMillis() - start;
+		log("Duration: " + duration + " mS");
+	}
+	
 	// PRIVATE
+	// Insert N random breaks in String.
+	private String breakString( int prefix, int N, String original ) {
+		char [] chars = original.toCharArray();
+		for ( int j = 0; j < N; j++ ) {
+			int breakChar = prefix + random.nextInt( chars.length - prefix );
+			chars[ breakChar ] = 'Z';
+		}
+		return new String( chars );
+	}
+	
 	private static void log(Object aMsg) {
 		System.out.println(String.valueOf(aMsg));
 	}
@@ -156,14 +210,25 @@ public final class ParallelSite {
 		URL url = new URL(aURL);
 		try {
 			URLConnection connection = url.openConnection();
-			int FIRST_LINE = 0;
-			String firstLine = connection.getHeaderField(FIRST_LINE);
-			result.success = true;
-			long end = System.currentTimeMillis();
-			result.timing = end - start;
-		} catch (IOException ex) {
+			HttpURLConnection httpConnection = (HttpURLConnection) connection;
+			httpConnection.setRequestMethod("GET");
+			int responseCode = httpConnection.getResponseCode();
+			// log( "   response code=" + responseCode );
+			// int FIRST_LINE = 0;
+			// log( "   content length=" + connection.getContentLength());
+			// String firstLine = connection.getHeaderField(FIRST_LINE);
+			if ( responseCode < 500 ) {
+				result.success = true;
+			} else {
+				result.success = false;
+			}
+		} catch (Exception ex) {
 			// ignore - fails
+			// log("Bad URL: " + ex.getCause());		
+			result.success = false;
 		}
+		long end = System.currentTimeMillis();
+		result.timing = end - start;
 		return result;
 	}
 
